e0bac21e165bd628ffbf49e1ae0878b19e6a34f8
oscar anillo <oscarani14@gmail.com>
2022-09-22 19:24:18 -0500

    add mobile view with data to show in the UI

---
+++ src/components/notification-component.js
@@ -0,0 +1,61 @@
+
+export default function SimpleNotification({
+  readMessageState,
+  avatar,
+  name,
+  text,
+  activity_post,
+  activity_game,
+  message,
+  timeStamp,
+  readMessage,
+}) {
+  
+
+  return (
+    <div>
+      {readMessage ? (
+        <div
+          className={`${readMessageState ? "notification" : "unread"}`}
+        >
+          <div className="notification-row">
+            <div>
+              <img src={avatar} alt="" />
+            </div>
+            <div>
+              <p>
+                <strong>{name}</strong> <span className="text">{text}</span>
+                <span className="activity-post">{activity_post}<span className="game">{activity_game}</span> 
+                {!readMessageState ? <div className="red-dot"></div> : " "}</span>
+                <br />
+                <span className="time">{timeStamp}</span>
+              </p>
+            </div>
+          </div>
+          <div className={`${message ? "message" : null}`}>
+            <p>{message}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="notification">
+          <div className="notification-row">
+            <div>
+              <img src={avatar} alt="" />
+            </div>
+            <div>
+              <p>
+                <strong>{name}</strong> <span className="text">{text}</span>
+                <span className="activity-post">{activity_post}<span className="game">{activity_game}</span></span>
+                <br />
+                <span className="time">{timeStamp}</span>
+              </p>
+            </div>
+          </div>
+          <div className={`${message ? "message" : null}`}>
+              <p>{message}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
